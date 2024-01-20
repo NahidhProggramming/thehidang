1810b2a66f3964d07948aa71f5b9fa96a206ff1d
NahidhProgrammig <nahidh2018@gmail.com>
2024-01-21 05:54:41 +0700

    first commit

---
+++ nahidhul/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.beranda, name='beranda'),
+    path('profil-kami', views.profil, name='profil'),
+    path('checkout', views.CheckoutView.as_view(), name='checkout'),
+    path('<slug:kategori_slug>/<slug:slug>', views.produk, name='produk'),
+    path('pemesanan-kami', views.pemesanan, name='pemesanan'),
+    path('hubungi-kami', views.KontakView.as_view(), name='kontak'),
+    path('cari', views.cari, name='cari'),
+    path('<slug:slug>', views.kategori, name='kategori'),
+    
+]
---
+++ umam/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UmamConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'umam'
